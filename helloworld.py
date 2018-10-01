84f044f949199ba36a33990a01393e9857dcf111
David Colombo <32454162+davidcolombogit@users.noreply.github.com>
2018-10-01 19:05:22 +0200

    Added "Hello World"
    Just Added "Hello World"

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,8 @@ lcd = lcddriver.lcd()
 
 first = raw_input('First Line LCD Text:\n')
 second = raw_input('Second Line LCD Text:\n')
-
+third = "Hello World";
 
 lcd.lcd_display_string(first, 1)
 lcd.lcd_display_string(second, 2)
+lcd.lcd_display_string(third, 3)
